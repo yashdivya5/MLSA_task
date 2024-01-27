be0462b48fb21b606cfbb9628ac5f7cb442cd8d2
yashdivya5 <121162537+yashdivya5@users.noreply.github.com>
2024-01-28 01:38:49 +0530

    changes name colour to white

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,7 +12,7 @@ const Home = ({ name, title }) => {
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
-        <h1>{name}</h1>
+      <h1 style={{ color: "white" }}>{name}</h1>
         <h2 style={{ color: "white" }}>{title}</h2> {/* Updated color to white */}
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
