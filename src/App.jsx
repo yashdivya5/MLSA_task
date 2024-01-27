f530ee9c1e0e7c297f7c82dadeca483c3eb3e3f8
yashdivya5 <121162537+yashdivya5@users.noreply.github.com>
2024-01-27 15:41:42 +0530

    all the images and names changed.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Yash Divya",
+  title: "Web Developer and Machine Learning Enthusiast",
+  email: "yashdivyapatna@gmail.com",
+  gitHub: "yashdivya5",
+  instagram: "yash.divya",
+  linkedIn: "https://www.linkedin.com/in/yash-divya-ab3aa6206/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "YashDivya3",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
