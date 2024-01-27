c44ba265e0d61a3ed9453c5f08eab7bb5167eeaa
yashdivya5 <121162537+yashdivya5@users.noreply.github.com>
2024-01-28 01:37:13 +0530

    changes colour to white

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,8 @@
-/**
- * Home component
- *
- * The section at the top of the page to display image of your
- * choice, name and title that describes your career focus.
- */
+// Inside Home.jsx file
 
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
-
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/jj-ying-8bghKxNU1j0-unsplash.jpg";
 
 const imageAltText = "Abstract image";
@@ -30,7 +13,7 @@ const Home = ({ name, title }) => {
       <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
-        <h2>{title}</h2>
+        <h2 style={{ color: "white" }}>{title}</h2> {/* Updated color to white */}
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
